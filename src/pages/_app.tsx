3a1f7ca11cac77bfde9575483e5b16627be48fba
Ryan <Redninja106@live.com>
2026-02-09 20:13:40 -0500

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,28 @@ const tasaExplorer = TASA_Explorer({
     fallback: ["Times New Roman"]
 })
 
+function Footer() {
+  return (
+    <div className='social-links'>
+      <hr/>
+      <a href='https://github.com/Redninja106' target='_blank' rel='noopener noreferrer' className='social-icon'>
+        <img src='/octicon.svg' alt='GitHub' />
+      </a>
+      <a href='https://www.linkedin.com/in/ryan-andersen1/' target='_blank' rel='noopener noreferrer' className='social-icon'>
+        <img src='/linkedin.svg' alt='LinkedIn' />
+      </a>
+      <a href='https://x.com/whos_wyan' target='_blank' rel='noopener noreferrer' className='social-icon'>
+        <img src='/x.svg' alt='X/Twitter' />
+      </a>
+    </div>
+  )
+}
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`content ${tasaExplorer.className}`}>
       <Component {...pageProps} />
+      <Footer></Footer>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,18 @@ function Gallery() {
                 
                 <a href="/projects/blockgame"><img src='blockgame/thumbnail.png'/></a>
                 <a href="/projects/spacerts"><img src='spacerts/thumbnail.png'/></a>
-                <a href="/projects/spacerts"><img src='spacerts/gameplay.png'/></a>
+                <a href="/projects/blockgame"><img src='blockgame/cave.png'/></a>
+                <a href="/projects/blockgame"><img src='blockgame/ores.png'/></a>
+                <a href="/projects/spacerts"><img src='spacerts/saturn.png'/></a>
+                <a href="/projects/spacerts"><img src='spacerts/jupiter.png'/></a>
 
                 <a href="/projects/blockgame"><img src='blockgame/thumbnail.png'/></a>
                 <a href="/projects/spacerts"><img src='spacerts/thumbnail.png'/></a>
-                <a href="/projects/spacerts"><img src='spacerts/gameplay.png'/></a>
-            
+                <a href="/projects/blockgame"><img src='blockgame/cave.png'/></a>
+                <a href="/projects/blockgame"><img src='blockgame/ores.png'/></a>
+                <a href="/projects/spacerts"><img src='spacerts/saturn.png'/></a>
+                <a href="/projects/spacerts"><img src='spacerts/jupiter.png'/></a>
+
             </div>
         </div>
     )
@@ -25,7 +31,7 @@ function Home() {
     });
     
     return (
-        <div>
+        <div className="main-page-content">
             <div className='title'>
                 Ryan Andersen
             </div>
@@ -39,15 +45,7 @@ function Home() {
             <span> or </span>
             <b><Link href='/resume'>Read my resume</Link></b>
             </p>
-            <p>
-            My links:
-            </p>
-            <ul>
-                <li><Link href='https://github.com/Redninja106'>GitHub</Link></li>
-                <li><Link href='https://www.linkedin.com/in/ryan-andersen1/'>LinkedIn</Link></li>
-                <li><Link href='https://x.com/whos_wyan'>X/Twitter</Link></li>
-            </ul>
-        </div>
+        </div> 
     )
 }
 
